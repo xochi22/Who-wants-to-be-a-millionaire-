80e09da07af74203fefad49caf0a38bfca379711
Bertrand Ugwuoke <bertrand51ugwuoke@gmail.com>
2022-09-25 03:21:48 +0100

    my react-quiz-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Quiz from './component/quiz'
+import data from './component/data'
+import Timer from './component/Timer';
 
 function App() {
+
+const [questionNumber,setQuestionNumber] = useState(1)
+const [stopTime,setStopTime] = useState(false)
+const[earned, setEarned] = useState("0$")
+
+
+  const moneyPyramid = [
+    {id:1, amount:"$ 10,000"},
+    {id:2, amount:"$ 20,000"},
+    {id:3, amount:"$ 30,000"},
+    {id:4, amount:"$ 40,000"},
+    {id:5, amount:"$ 50,000"},
+    {id:6, amount:"$ 100,000"},
+    {id:7, amount:"$ 200,000"},
+    {id:8, amount:"$ 300,000"},
+    {id:9, amount:"$ 400,000"},
+    {id:10,amount:"$ 500,000"},
+    {id:11,amount:"$ 1,000,000,000"},
+    {id:12,amount:"$ 2,000,000,000"},
+   
+    ].reverse();
+
+    useEffect(() =>{
+      questionNumber > 1 &&
+      setEarned (moneyPyramid.find(m => m.id === questionNumber).amount)
+    },[moneyPyramid, questionNumber])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+    <div className="main">
+     {stopTime ? <h1 className='endText'> You Won : {earned} </h1> : (
+       <>
+       <div className="top">
+        <div className="timer"> <Timer setStopTime={setStopTime} 
+        questionNumber={questionNumber}/>
+        </div>
+        </div>
+        <div className="bottom">
+         <Quiz 
+          data={data}
+          setStopTime={setStopTime}
+         questionNumber={questionNumber} 
+         setQuestionNumber={setQuestionNumber}
+         />
+         </div> 
+         </>       
+     )}
+   
+      </div>
+      <div className="pyramid">
+      <ul className="moneyList">
+        {moneyPyramid.map(m => <li className={questionNumber===m.id ? "moneyListItem active" : "moneyListItem"}>
+        <span className="moneyListItemNumber">{m.id}</span>
+        <span className="moneyListItemAmount">{m.amount}</span>
+          </li>
+          )}
+          </ul>
+          </div>
+        </div>
+     
   );
 }
 
---
+++ src/component/data.js
@@ -0,0 +1,327 @@
+const data = [
+    {
+        id : 1,
+        question : "Who won the next rated artiste in the just concluded headies?",
+        answer : [
+            {
+                text : "Ruger",
+                correct : false
+            },
+
+            {
+                text : "Zinoleesky",
+                correct : false
+            },
+
+            {
+                text : "Bnxn",
+                correct : true
+            },
+
+            {
+                text : "Ayra Starr",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 2,
+        question : "The Manchester united anthem GOD ABEG was composed by?",
+        answer : [
+            {
+                text : "Ludwig van Beethoven",
+                correct : false
+            },
+
+            {
+                text : "Wolfgang Amadeus Mozart",
+                correct : false
+            },
+
+            {
+                text : "Richard Wagner",
+                correct : false
+            },
+
+            {
+                text : "portable",
+                correct : true
+            },
+
+        ]
+    },
+
+    {
+        id : 3,
+        question : "Emi Lo Kan is a slogan associated with",
+        answer : [
+            {
+                text : "Peter Obi",
+                correct : false
+            },
+
+            {
+                text : "Bola Ahmed Tinubu",
+                correct : true
+            },
+
+            {
+                text : "Atiku Abubakar",
+                correct : false
+            },
+
+            {
+                text : "Rabiu Kwankwaso",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 4,
+        question : "GO AND VERIFY is a catch associated with",
+        answer : [
+            {
+                text : "Bola Ahmed Tinubu",
+                correct : false
+            },
+
+            {
+                text : "Rabiu Kwankwaso",
+                correct : false
+            },
+
+            {
+                text : "Atiku Abubakar",
+                correct : false
+            },
+
+            {
+                text : "Peter Obi",
+                correct : true
+            },
+
+        ]
+    },
+
+    {
+        id : 5,
+        question : "Who is known as the unifier in the upcoming General Elections?",
+        answer : [
+            {
+                text : "Atiku Abubakar",
+                correct : true
+            },
+
+            {
+                text : "Peter Obi",
+                correct : false
+            },
+
+            {
+                text : "Bola Ahmed Tinubu",
+                correct : false
+            },
+
+            {
+                text : "Rabiu Kwankwaso",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 6,
+        question : "The twitter influencer spear-heading the  war against FINISHED MEN is ",
+        answer : [
+            {
+                text : "Malik",
+                correct : false
+            },
+
+            {
+                text : "Shola",
+                correct : true
+            },
+
+            {
+                text : "Boj",
+                correct : false
+            },
+
+            {
+                text : "Ruth",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 7,
+        question : "Queen Elizabeth II was the longest serving monarch by",
+        answer : [
+            {
+                text : "40 years",
+                correct : false
+            },
+
+            {
+                text : "20 years",
+                correct : false
+            },
+
+            {
+                text : "70 years",
+                correct : true
+            },
+
+            {
+                text : "96 years",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 8,
+        question : "The twitter fanbase known for VAWULENCE is",
+        answer : [
+            {
+                text : "WIZKID FC",
+                correct : true
+            },
+
+            {
+                text : "30BG",
+                correct : false 
+            },
+
+            {
+                text : "RAVERS",
+                correct : false
+            },
+
+            {
+                text : "CELESTIALS",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 9,
+        question : "The Governor with the best band in Nigeria is",
+        answer : [
+            {
+                text : "GOV. Ifeanyi Okowa",
+                correct : false
+            },
+
+            {
+                text : "GOV. El-Rufai",
+                correct : false
+            },
+
+            {
+                text : "GOV. Sanwo-olu",
+                correct : false
+            },
+
+            {
+                text : "GOV. Nyesom Wike",
+                correct : true
+            },
+
+        ]
+    },
+
+    {
+        id : 10,
+        question : "The Genius behind the body of work HOUSE OF THE DRAGON is",
+        answer : [
+            {
+                text : "George R.R Martin",
+                correct : true
+            },
+
+            {
+                text : "Matt Smith",
+                correct : false
+            },
+
+            {
+                text : "Paddy Cosidine",
+                correct : false
+            },
+
+            {
+                text : "kit Harrington",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 11,
+        question : "A fictious name used to describe an unknown male person is",
+        answer : [
+            {
+                text : "Rachael Solando",
+                correct : false
+            },
+
+            {
+                text : "Korencee",
+                correct : false
+            },
+
+            {
+                text : "Peter Agabi",
+                correct : false
+            },
+
+            {
+                text : "John Doe",
+                correct : true
+            },
+
+        ]
+    },
+    {
+        id : 12,
+        question : "The correct twitter handle for this developer is ",
+        answer : [
+            {
+                text : "@officialcr7",
+                correct : false
+            },
+
+            {
+                text : "@therealfemiotedola",
+                correct : false
+            },
+
+            {
+                text : "_@xochi22",
+                correct : true
+            },
+
+            {
+                text : "@MrMoney",
+                correct : false
+            },
+
+        ]
+    },
+
+
+] 
+export default data;
